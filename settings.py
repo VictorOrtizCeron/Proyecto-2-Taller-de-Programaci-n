6d1d19b930102ef798072095a4664821af48c34b
MoonFoxCake <ororap7@gmail.com>
2021-11-14 17:01:01 -0600

    commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+TITLE = "Mrrp"
+WIDTH = 800
+HEIGHT = 600
+FPS = 30
+
+#definicion de colores utiles
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
