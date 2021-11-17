db5ba56e3de628b9f010db7d89d61bcb70ad56b5
MoonFoxCake <ororap7@gmail.com>
2021-11-17 14:13:45 -0600

    commit on 'master'

---
+++ Funciones_Basicas.py
@@ -0,0 +1,55 @@
+"""
+ Instituto Tecnológico de Costa Rica
+                    Ingeniería en Computadores
+
+    Lenguaje: Python 3.9.6
+    Autores:  Victor Ortiz Ceron y Jose Ignacio Castillo 
+    Co-Autor: Jose Fernando Morales Vargas
+    Versión: 1.0
+    Fecha última Modificación: 22/10/2021
+    Módulo: Funciones_Básicas
+    Descripción del módulo: Este es un módulo auxiliar que contiene funciones básicas que pueden ser utilizadas en cualquier etapa del programa,
+    por lo que es mejor mantenerlas aparte e importarlas cuando sea necesario utilizarlas. Dichas funciones en lus totalidad fueron creadas por
+    el asistente Jose Fernando Morales Vargas durante las sesiones de Taller de programación, y posteriormente modificadas por Víctor Ortiz para 
+    acomodar las necesidades del programa.
+"""
+#Importación de las bibliotecas necesarias para el funcionamiento de las funciones. 
+from tkinter import *
+from os import path
+from random import randint
+import vlc
+
+#Inicia el media player de vlc.
+reproductor = vlc.MediaPlayer()
+
+#Es una funcion que encuentra una imagen utilizando su dirección relativa en el directorio, y la retorna.
+def cargar_img(nombre):
+    ruta  = path.join('Assets/', nombre)
+    img=PhotoImage(file=ruta)
+    return img
+
+#Función que encuentra un mp3 utilizando su dirección relativa.
+def cargarMP3(nombre):
+    return path.join('Assets/',nombre)
+
+#Funcion que detiene la reproducción de un mp3
+def detener_cancion():
+    global reproductor
+    if (isinstance(reproductor,vlc.MediaPlayer)):
+        reproductor.stop()
+
+#Funcion que reproduce un mp3 y detiene el anterior, si había otro sonando.         
+def reproducir_cancion(archivoMP3):
+    global reproductor
+    detener_cancion()
+    reproductor = vlc.MediaPlayer(archivoMP3)
+    reproductor.audio_set_volume(60)
+    reproductor.play()
+
+#Función que hace lo mismo que reproducir_cancion pero delimitado para sonidos cortos y efectos.
+def reproducir_fx(archivoMP3):
+    global reproductor
+    detener_cancion()
+    reproductor = vlc.MediaPlayer(archivoMP3)
+    reproductor.audio_set_volume(60)
+    reproductor.play()
---
+++ Funciones_Scoreboard.py
@@ -0,0 +1,65 @@
+
+
+
+
+def show_scores(current_score):
+    #Funcion para printear resultados en la pantalla despues del juego y poder volver al menu principal
+    global name
+    Nombre = name.get()
+    Punt_fin = f"""
+    Gracias por 
+    
+    jugar Submarine escape! 
+    
+    He aca los datos de su partida
+    
+    Puntaje:
+    {current_score}
+    
+    Nombre:
+    {Nombre}
+    """
+
+    return Punt_fin
+
+def get_datas(current_score):
+    save_data()
+    with open("scores.txt", "r") as file:
+        # Leer y dividir la informacion para dar formato
+        scores = [n.split("__") for n in file.read().split("\n") if len(n) >= 4]
+        # Organizar la lista de puntajes
+        scores.sort(key=lambda i: int(i[1]))
+        # Arreglo para invertir la lista de puntajes, para asi poder mostrar de mayor a menor
+        scores = list(reversed(scores))
+
+    if len(scores) > 10:
+        scores = scores[:10]
+        #aca, se hace que el programa se muestre solo los 10 mejores resultados
+
+    def format_name(name,score):
+        if len(name) > 10:
+            name = name[:10] + "..."
+        goal = 13 - len(name)
+        total_name = name+" "*goal
+        return f"NAME: {total_name} | SCORE: {score}"
+
+    scores = "\n".join(format_name(n,s) for n, s in scores)
+    scores_formatted = f"""
+    Felicidades, su puntaje es de: {current_score}
+{scores}
+    """
+    return scores_formatted
+
+
+def save_data():
+    global name, Diff 
+    name_info = name.get()
+
+    with open("scores.txt", "r") as f:
+        datas = f.read()
+
+    datas += name_info + "__" + str(Scr) + "\n"
+
+    with open("scores.txt", "w") as f:
+        f.write(datas)
+
