88e86969e09da41722a8c850f1329402163b4540
MoonFoxCake <ororap7@gmail.com>
2021-11-24 17:50:59 -0600

    commit

--- Scoreboard.py
+++ Scoreboard.py
@@ -19,18 +19,9 @@ def Score_Board():
     L_about = Label(
         Result,
         text=get_datas(Scr),
-        font=("Consolas", 12),
+        font=("8BIT WONDER",12),
     )
 
-    Back = Button(
-        Result,
-        text="Menu Principal",
-        bg="black",
-        fg="white",
-        font="Arial",
-        command=Ir_a_Menu,
-    )
-    L_about.place(x=50, y=50)
-    Back.place(x=110, y=360)
+
 
     Result.mainloop()
