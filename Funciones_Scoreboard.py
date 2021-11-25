7b1ada0876f7266a8040bb51b5afd4b5c38eeed6
MoonFoxCake <ororap7@gmail.com>
2021-11-24 19:30:24 -0600

    commit

--- Funciones_Scoreboard.py
+++ Funciones_Scoreboard.py
@@ -1,10 +1,26 @@
+"""
+ Instituto Tecnológico de Costa Rica
+                    Ingeniería en Computadores
 
+    Lenguaje: Python 3.9.6
+    Autores:  Victor Ortiz Ceron(vortiz@estudiantec.cr) , José Ignacio Castillo()
+    Tutor: Ramses Gutiérrez
+    Versión: 1.0
+    Fecha última Modificación: 24/11/2021
+    Módulo: Funciones_Scoreboard.py
+    Descripción del módulo: Este modulo se encarga de poder hacer que la scoreboard funcione, en este caso usando get datas y read data para poder extraer la informacion de puntajes y
+    ordenarla para el display que lo hace el scoreboard.py, 
+    Entradas: Score, Name y scores.txt
+    Salidas: Escribir en scores.txt, y retornar la informacion de los puntajes en formato de string.
+"""
 def get_datas(current_score):
     with open("scores.txt", "r") as file:
         # Leer y dividir la informacion para dar formato
         scores = read_data_recurse(open("scores.txt",'r').readlines())
+        #abrir el archivo en texto y leerlo
         # Organizar la lista de puntajes
         scores.sort(key=lambda x: int(x[1]))
+        #Key se usa como una herramienta para ordenar la lista de puntajes
         # Arreglo para invertir la lista de puntajes, para asi poder mostrar de mayor a menor
         scores = list(reversed(scores))
 
@@ -25,26 +41,32 @@ def get_datas(current_score):
     """
     return scores_formatted
 
+#data_recurse es una funcion que se encarga de leer el archivo de texto y dividir la informacion en una lista de tuplas
 def read_data_recurse(lines, scores=[]):
   if len(lines) == 0:
     return scores
   elif len(scores) > 10:
     return scores
+#funcion recrusiva la cual lee las lineas del archivo del texto y las divide
   else:
     n = lines[0]
     if len(n) >= 4:
       scores.append(n.split("__"))
+    #aca se divide la informacion usando el underscore como separador
     return read_data_recurse(lines[1:],scores)
 
+
+#save_data es una funcion para poder guardar los datos en el archivo de texto
 def save_data(name, score):
     name_info = name
 
     with open("scores.txt", "r") as f:
         datas = f.read()
-
+        #leer el archivo de texto y guardarlo en una variable
     datas += name_info + "__" + str(score) + "\n"
+    #concatenar la informacion del nombre y el puntaje
 
     with open("scores.txt", "w") as f:
         f.write(datas)
-
+        #escribir la informacion en el archivo de texto
 
--- Scoreboard.py
+++ Scoreboard.py
@@ -1,3 +1,18 @@
+"""
+ Instituto Tecnológico de Costa Rica
+                    Ingeniería en Computadores
+
+    Lenguaje: Python 3.9.6
+    Autores:  Victor Ortiz Ceron(vortiz@estudiantec.cr) , José Ignacio Castillo()
+    Tutor: Ramses Gutiérrez
+    Versión: 1.0
+    Fecha última Modificación: 24/11/2021
+    Módulo: Funciones_Scoreboard.py
+    Descripción del módulo: Este modulo es el scoreboard en si, el cual gracias a Funciones_Scoreboard 
+    Entradas: Informacion de 'get_datas' de funciones_scoreboard.py
+    Salidas: display de la scoreboard
+"""
+
 from tkinter import *
 from os import path
 import vlc
@@ -9,6 +24,7 @@ from Funciones_Scoreboard import *
 from Ingame import Score as Scr
 
 def Score_Board():
+    #funcion la cual crea una ventana con la scoreboard
     def Ir_a_Menu():
         Result.destroy()
 
@@ -16,10 +32,12 @@ def Score_Board():
     Result.title("Altos puntajes")
     Result.minsize(375, 420)
     Result.resizable(width=NO, height=NO)
+    #Configuracion de la ventana
     L_about = Label(
         Result,
         text=get_datas(Scr),
-        font=("8BIT WONDER",12),
+        font=("8BIT WONDER",15),
+        #aca se llama a get_datas de funciones_scoreboard.py para la informacion de scores.txt y se le da formato
     )
 
 
