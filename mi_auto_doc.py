09afac1508a760c87fd033421bff7d24b49a4602
Victor <vortizc@estudiantec.cr>
2021-11-25 04:41:36 -0600

    mi_auto_doc

---
+++ mi_auto_doc.py
@@ -0,0 +1,20 @@
+import Menu_Principal
+import Funciones_Basicas
+import Funciones_Scoreboard
+import Control_Pantallas
+import Ingame
+import Ingame2
+import Ingame3
+import Scoreboard
+ 
+def mi_auto_doc():
+    print(Menu_Principal.__doc__)
+    print(Funciones_Basicas.__doc__)
+    print(Funciones_Scoreboard.__doc__)
+    print(Control_Pantallas.__doc__)
+    print(Ingame.__doc__)
+    print(Ingame2.__doc__)
+    print(Ingame3.__doc__)
+    print(Scoreboard.__doc__)
+
+#mi_auto_doc()
