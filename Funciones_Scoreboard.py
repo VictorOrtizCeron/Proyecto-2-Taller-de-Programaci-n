84e67bff23b622198503f6b005fc8261b6d58d1e
MoonFoxCake <ororap7@gmail.com>
2021-11-24 20:33:49 -0600

    commit

--- Funciones_Scoreboard.py
+++ Funciones_Scoreboard.py
@@ -1,3 +1,49 @@
+def get_datas(current_score):
+    with open("scores.txt", "r") as file:
+        scores = read_data_recurse(open("scores.txt",'r').readlines())
+        scores.sort(key=lambda x: int(x[1]))
+        scores = list(reversed(scores))
+
+    if len(scores) > 10:
+        scores = scores[:10]
+
+    def format_name(name,score):
+        if len(name) > 10:
+            name = name[:10] + "..."
+        goal = 13 - len(name)
+        total_name = name+" "*goal
+        return f"NAME: {total_name} | SCORE: {score}"
+
+    scores = "\n".join(format_name(n,s) for n, s in scores)
+    scores_formatted = f"""
+{scores}
+    """
+    return scores_formatted
+
+def read_data_recurse(lines, scores=[]):
+  if len(lines) == 0:
+    return scores
+  elif len(scores) > 10:
+    return scores
+  else:
+    n = lines[0]
+    if len(n) >= 4:
+      scores.append(n.split("__"))
+    return read_data_recurse(lines[1:],scores)
+#save_data es una funcion para poder guardar los datos en el archivo de texto
+def save_data(name, score):
+    name_info = name
+
+    with open("scores.txt", "r") as f:
+        datas = f.read()
+        #leer el archivo de texto y guardarlo en una variable
+    datas += name_info + "__" + str(score) + "\n"
+    #concatenar la informacion del nombre y el puntaje
+
+    with open("scores.txt", "w") as f:
+        f.write(datas)
+        #escribir la informacion en el archivo de texto
+
 """
  Instituto Tecnológico de Costa Rica
                     Ingeniería en Computadores
@@ -13,60 +59,3 @@
     Entradas: Score, Name y scores.txt
     Salidas: Escribir en scores.txt, y retornar la informacion de los puntajes en formato de string.
 """
-def get_datas(current_score):
-    with open("scores.txt", "r") as file:
-        # Leer y dividir la informacion para dar formato
-        scores = read_data_recurse(open("scores.txt",'r').readlines())
-        #abrir el archivo en texto y leerlo
-        # Organizar la lista de puntajes
-        scores.sort(key=lambda x: int(x[1]))
-        #Key se usa como una herramienta para ordenar la lista de puntajes
-        # Arreglo para invertir la lista de puntajes, para asi poder mostrar de mayor a menor
-        scores = list(reversed(scores))
-
-    if len(scores) > 10:
-        scores = scores[:10]
-        #aca, se hace que el programa se muestre solo los 10 mejores resultados
-
-    def format_name(name,score):
-        if len(name) > 10:
-            name = name[:10] + "..."
-        goal = 13 - len(name)
-        total_name = name+" "*goal
-        return f"NAME: {total_name} | SCORE: {score}"
-
-    scores = "\n".join(format_name(n,s) for n, s in scores)
-    scores_formatted = f"""
-{scores}
-    """
-    return scores_formatted
-
-#data_recurse es una funcion que se encarga de leer el archivo de texto y dividir la informacion en una lista de tuplas
-def read_data_recurse(lines, scores=[]):
-  if len(lines) == 0:
-    return scores
-  elif len(scores) > 10:
-    return scores
-#funcion recrusiva la cual lee las lineas del archivo del texto y las divide
-  else:
-    n = lines[0]
-    if len(n) >= 4:
-      scores.append(n.split("__"))
-    #aca se divide la informacion usando el underscore como separador
-    return read_data_recurse(lines[1:],scores)
-
-
-#save_data es una funcion para poder guardar los datos en el archivo de texto
-def save_data(name, score):
-    name_info = name
-
-    with open("scores.txt", "r") as f:
-        datas = f.read()
-        #leer el archivo de texto y guardarlo en una variable
-    datas += name_info + "__" + str(score) + "\n"
-    #concatenar la informacion del nombre y el puntaje
-
-    with open("scores.txt", "w") as f:
-        f.write(datas)
-        #escribir la informacion en el archivo de texto
-
--- Scoreboard.py
+++ Scoreboard.py
@@ -24,22 +24,27 @@ from Funciones_Scoreboard import *
 from Ingame import Score as Scr
 
 def Score_Board():
-    #funcion la cual crea una ventana con la scoreboard
     def Ir_a_Menu():
         Result.destroy()
-
     Result = Toplevel()
-    Result.title("Altos puntajes")
-    Result.minsize(375, 420)
+    Result.title("Resultados Partida")
+    Result.minsize(420, 530)
     Result.resizable(width=NO, height=NO)
-    #Configuracion de la ventana
     L_about = Label(
         Result,
         text=get_datas(Scr),
-        font=("8BIT WONDER",15),
-        #aca se llama a get_datas de funciones_scoreboard.py para la informacion de scores.txt y se le da formato
+        font=("Consolas", 10),
     )
 
-
+    Back = Button(
+        Result,
+        text="Menu Principal",
+        bg="black",
+        fg="white",
+        font="Arial",
+        command=Ir_a_Menu,
+    )
+    L_about.place(x=50, y=50)
+    Back.place(x=110, y=360)
 
     Result.mainloop()
